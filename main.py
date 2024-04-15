f63bd87e3f1ea5ec95427bd43c1537e20b8c576b
Breno Mendes <b.mendesbernardo2020@gmail.com>
2024-04-15 19:40:08 -0300

    build: Script base para web scraping com Selenium

---
+++ main.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedrive está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_option.add_argument(option) # type: ignore
+
+    chrome_service = Service(
+        # executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options,
+    )
+
+    return browser
+
+if __name__ == '__main__':
+    # Example
+    # options = '--headless', '--disable-gpu'
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://gloogle.com')
+
+    # Dorme por 10s
+    sleep(10)
