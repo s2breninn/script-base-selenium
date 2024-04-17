b1fba5562cf524d1dde2c91445daeb1e1f8b1177
Breno Mendes <89087720+s2breninn@users.noreply.github.com>
2024-04-17 12:54:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 # Chrome Options
 # https://peter.sh/experiments/chromium-command-line-switches/
@@ -13,32 +17,55 @@ ROOT_FOLDER = Path(__file__).parent
 CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
 
 def make_chrome_browser(*options: str) -> webdriver.Chrome:
-    chrome_options = webdriver.ChromeOptions()
+    try:
+        chrome_options = webdriver.ChromeOptions()
 
-    # chrome_options.add_argument('--headless')
-    if options is not None:
-        for option in options:
-            chrome_option.add_argument(option) # type: ignore
+        # chrome_options.add_argument('--headless')
+        if options is not None:
+            for option in options:
+                chrome_option.add_argument(option) # type: ignore
 
-    chrome_service = Service(
-        # executable_path=str(CHROME_DRIVER_PATH),
-    )
+        chrome_service = Service(
+            executable_path=str(CHROME_DRIVER_PATH),
+        )
 
-    browser = webdriver.Chrome(
-        service=chrome_service,
-        options=chrome_options,
-    )
+        browser = webdriver.Chrome(
+            service=chrome_service,
+            options=chrome_options,
+        )
+    except ExceptionGroup as e:
+        print(f'Error: {e}')
 
     return browser
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
     # Example
     # options = '--headless', '--disable-gpu'
     options = ()
     browser = make_chrome_browser(*options)
 
     # Como antes
-    browser.get('https://gloogle.com')
+    browser.get('https://www.google.com/')
+
+    # Espere para encontrar o input
+    try:
+        search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+            ec.presence_of_element_located(
+                (By.NAME, 'q')
+            )
+        )
+    except ExceptionGroup as e:
+        print(f'Elemento não identificado: {e}')
+
+    search_input.send_keys('Hello world')
+    search_input.send_keys(Keys.ENTER)
+
+    results = browser.find_element(By.ID, 'search')
+    links = results.find_elements(By.TAG_NAME, 'a')
+    links[0].click()
+    print(links)
 
     # Dorme por 10s
-    sleep(10)
+    sleep(TIME_TO_WAIT)
